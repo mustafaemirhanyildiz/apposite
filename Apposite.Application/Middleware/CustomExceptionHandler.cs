1077f4534655ffa8034eee2ad6ce90fa6e9fa6da
Taha Muslu <taha-_muslu1216@hotmail.com>
2024-03-03 13:55:44 +0300

    feat: new response

---
+++ Apposite.Application/Middleware/CustomExceptionHandler.cs
@@ -0,0 +1,50 @@
+﻿using Apposite.Core.Dtos;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+
+namespace Apposite.Application.Middleware
+{
+    public class CustomExceptionHandler
+    {
+        private readonly RequestDelegate _next;
+        public CustomExceptionHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+
+                Log.Information($"Request {httpContext.Request.Path} is called ");
+                await _next(httpContext); // gelen datayı da logla
+                Log.Information($"Request {httpContext.Request.Method} {httpContext.Request.Path} => {httpContext.Response.StatusCode} is finished");
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+        private async Task HandleExceptionAsync(HttpContext httpContext, Exception ex)
+        {
+            httpContext.Response.ContentType = "application/json";
+            Response<object> responseModel = Response<object>.Fail(ex.Message, 500);
+
+            if (ex is UnauthorizedAccessException)
+            {
+                //responseModel.Message = ResponseMessage.unauthorized.ToString();
+                responseModel.StatusCode = 401;
+                Log.Error($"Unauthorized Access: {ex.Message}  - - " +
+                                 $"Request  {httpContext.Request.Method} {httpContext.Request.Path}");
+            }
+            else
+            {
+                Log.Error($"Internal Server Error: {ex.Message}  - - " +
+                                 $"Request  {httpContext.Request.Method} {httpContext.Request.Path}");
+            }
+
+            httpContext.Response.StatusCode = responseModel.StatusCode;
+            await httpContext.Response.WriteAsJsonAsync(responseModel,typeof(Response<object>));
+        }
+    }
+}
---
+++ Apposite.Core/Dtos/MyResponse.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.Json.Serialization;
+using Apposite.Core.Pagination;
+using FluentValidation.Results;
+
+namespace Apposite.Core.Dtos
+{
+    public class MyResponse<T>
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public T? Data { get; set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+
+        [JsonIgnore]
+        public bool IsSuccessful { get; set; }
+
+        public string? Message { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Pager Pagination { get; set; }
+
+        // Static Factory Method
+        public static MyResponse<T> Success(T data, int statusCode, Pager pager = null)
+        {
+            return new MyResponse<T> { Data = data, StatusCode = statusCode, IsSuccessful = true, Pagination = pager };
+        }
+
+        public static MyResponse<T> Success(int statusCode)
+        {
+            return new MyResponse<T> { Data = default(T), StatusCode = statusCode, IsSuccessful = true };
+        }
+
+        public static MyResponse<T> Fail(string message, int statusCode)
+        {
+            return new MyResponse<T>
+            {
+                Message = message,
+                StatusCode = statusCode,
+                IsSuccessful = false
+            };
+        }
+
+        public static MyResponse<T> Fail(ValidationResult result, int statusCode)
+        {
+            return new MyResponse<T>
+            {
+                Message = string.Join(";", result.Errors.Select(x => x.ErrorMessage).ToList()),
+                StatusCode = statusCode,
+                IsSuccessful = false
+            };
+        }
+
+    }
+}
