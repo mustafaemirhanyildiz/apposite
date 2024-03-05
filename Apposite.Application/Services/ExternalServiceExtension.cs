136b7d5b09550dcab816b991c29f83f631f3f820
Taha Muslu <taha-_muslu1216@hotmail.com>
2024-03-05 20:41:39 +0300

    fix: token service DI

--- Apposite.Application/Services/ExternalServiceExtension.cs
+++ Apposite.Application/Services/ExternalServiceExtension.cs
@@ -1,5 +1,6 @@
 
 using Apposite.Application.Services.IngredientService;
+using Apposite.Application.Services.TokenService;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Apposite.Application.Services
@@ -9,6 +10,7 @@ namespace Apposite.Application.Services
         public static void ExternalServices(this IServiceCollection services)
         {
             services.AddSingleton<IIngredientService, IngredientService.IngredientService>();
+            services.AddScoped<ITokenService, TokenService.TokenService>();
         }
 
     }
