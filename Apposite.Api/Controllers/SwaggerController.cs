d9331da27a0b125feb7bbe6ab3556f0f887072d4
Taha Muslu <taha-_muslu1216@hotmail.com>
2024-03-10 11:37:33 +0300

    feat: swagger controller add

---
+++ Apposite.Api/Controllers/SwaggerController.cs
@@ -0,0 +1,21 @@
+﻿using Apposite.Core.ControllerBases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("/")]
+    public class SwaggerController : CustomControllerBase
+    {
+
+
+        [HttpGet, Route("")]
+        public void Swagger()
+        {
+            Response.Redirect("/swagger/index.html");
+        }
+
+
+    }
+}
