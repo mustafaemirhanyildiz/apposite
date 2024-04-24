78504627a7efa86ecdd4b964ebd3d4f157decb8d
Taha Muslu <taha-_muslu1216@hotmail.com>
2024-04-24 16:30:21 +0300

    fix: elastic starter fix

--- Apposite.Application/Services/StarterSqlService.cs
+++ Apposite.Application/Services/StarterSqlService.cs
@@ -1,11 +1,18 @@
-﻿using Apposite.Application.Dto.ElasticSearch.CuisinePreference;
+﻿using Apposite.Application.Dto.CuisinePreference;
+using Apposite.Application.Dto.ElasticSearch.CuisinePreference;
 using Apposite.Application.Dto.ElasticSearch.Health;
 using Apposite.Application.Dto.ElasticSearch.Ingredient;
+using Apposite.Application.Dto.Health;
+using Apposite.Application.Dto.Ingredient;
+using Apposite.Application.Mapping;
 using Apposite.Application.Services.CuisinePreferenceService;
 using Apposite.Application.Services.HealthService;
 using Apposite.Application.Services.IngredientService;
+using Apposite.Application.Settings;
 using Apposite.Persistence;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Nest;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,18 +32,27 @@ namespace Apposite.Application.Services
             var cuisinePreferenceService = scope.ServiceProvider.GetRequiredService<ICuisinePreferenceService>();
             var healthService = scope.ServiceProvider.GetRequiredService<IHealthService>();
             var ingredientService = scope.ServiceProvider.GetRequiredService<IIngredientService>();
+            var elasticClient = scope.ServiceProvider.GetRequiredService<IElasticClient>();
+            var _elasticSettings = scope.ServiceProvider.GetRequiredService<IOptions<ElasticSettings>>().Value;
 
-            var cuisines = dbContext.CuisinePreferences.ToList();
-            var healths = dbContext.Healths.ToList();
-            var ingredients = dbContext.Ingredients.ToList();
-
-            var cuisinesDto = new List<CreateElasticCuisinePreferenceDto>();
-            var healthsDto = new List<CreateElasticHealthDto>();
-            var ingredientsDto = new List<CreateElasticIngredientDto>();
-
-            await cuisinePreferenceService.CreateCuisinePreferenceBulkAsync(cuisinesDto);
-            await healthService.CreateHealthBulkAsync(healthsDto);
-            await ingredientService.CreateIngredientBulkAsync(ingredientsDto);
+            if ((await elasticClient.SearchAsync<CuisinePreferenceDto>(s => s.Index(_elasticSettings.CuisinePreferenceIndex).Size(1))).Total == 0)
+            {
+                var cuisines = dbContext.CuisinePreferences.ToList();
+                var cuisinesDto = ObjectMapper.Mapper.Map<List<CreateElasticCuisinePreferenceDto>>(cuisines);
+                await cuisinePreferenceService.CreateCuisinePreferenceBulkAsync(cuisinesDto);
+            }
+            if ((await elasticClient.SearchAsync<HealthDto>(s => s.Index(_elasticSettings.HealthIndex).Size(1))).Total == 0)
+            {
+                var healths = dbContext.Healths.ToList();
+                var healthsDto = ObjectMapper.Mapper.Map<List<CreateElasticHealthDto>>(healths);
+                await healthService.CreateHealthBulkAsync(healthsDto);
+            }
+            if ((await elasticClient.SearchAsync<IngredientDto>(s => s.Index(_elasticSettings.IngredientIndex).Size(1))).Total == 0)
+            {
+                var ingredients = dbContext.Ingredients.ToList();
+                var ingredientsDto = ObjectMapper.Mapper.Map<List<CreateElasticIngredientDto>>(ingredients);
+                await ingredientService.CreateIngredientBulkAsync(ingredientsDto);
+            }
         }
     }
 }
