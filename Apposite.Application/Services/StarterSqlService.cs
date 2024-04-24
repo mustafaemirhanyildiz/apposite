e4954ad4eeb720068afb33606f858b912a215ce1
Taha Muslu <taha-_muslu1216@hotmail.com>
2024-04-24 15:57:20 +0300

    feat: auto add elastic data

---
+++ Apposite.Application/Services/StarterSqlService.cs
@@ -0,0 +1,42 @@
+﻿using Apposite.Application.Dto.ElasticSearch.CuisinePreference;
+using Apposite.Application.Dto.ElasticSearch.Health;
+using Apposite.Application.Dto.ElasticSearch.Ingredient;
+using Apposite.Application.Services.CuisinePreferenceService;
+using Apposite.Application.Services.HealthService;
+using Apposite.Application.Services.IngredientService;
+using Apposite.Persistence;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace Apposite.Application.Services
+{
+    public static class StarterSqlService
+    {
+
+        public async static Task StarterSync(IServiceProvider _serviceProvider)
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<AppositeDbContext>();
+            var cuisinePreferenceService = scope.ServiceProvider.GetRequiredService<ICuisinePreferenceService>();
+            var healthService = scope.ServiceProvider.GetRequiredService<IHealthService>();
+            var ingredientService = scope.ServiceProvider.GetRequiredService<IIngredientService>();
+
+            var cuisines = dbContext.CuisinePreferences.ToList();
+            var healths = dbContext.Healths.ToList();
+            var ingredients = dbContext.Ingredients.ToList();
+
+            var cuisinesDto = new List<CreateElasticCuisinePreferenceDto>();
+            var healthsDto = new List<CreateElasticHealthDto>();
+            var ingredientsDto = new List<CreateElasticIngredientDto>();
+
+            await cuisinePreferenceService.CreateCuisinePreferenceBulkAsync(cuisinesDto);
+            await healthService.CreateHealthBulkAsync(healthsDto);
+            await ingredientService.CreateIngredientBulkAsync(ingredientsDto);
+        }
+    }
+}
