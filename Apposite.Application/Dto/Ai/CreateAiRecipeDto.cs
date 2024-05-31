0b320259e1a268fd124a10d37578c17ebc4d6898
Taha Muslu <taha-_muslu1216@hotmail.com>
2024-05-31 14:00:52 +0300

    feat: create ai dto update

--- Apposite.Application/Dto/Ai/CreateAiRecipeDto.cs
+++ Apposite.Application/Dto/Ai/CreateAiRecipeDto.cs
@@ -9,6 +9,7 @@ namespace Apposite.Application.Dto.Ai
 {
     public class CreateAiRecipeDto
     {
+        public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
